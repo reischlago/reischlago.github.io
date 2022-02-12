a2d140b7edde44776aa80039005af3976cc15246
Agoston Reischl <ragoston001@gmail.com>
2022-02-12 22:38:39 +0100

    background change on scroll

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ $(window).scroll(function() {
         $("#topHeader").removeClass("scroll");
     }
 });
+
+$(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+	var elementOffset = $(".bg-scroll-to-primary").offset().top;
+	var height = $(window).height();
+	var distance = (elementOffset - scrollTop);
+	if(distance <= height - 200) {
+		$(".bg-scroll-to-primary").addClass("in-view");
+	}
+	else {
+		$(".bg-scroll-to-primary").removeClass("in-view");
+	}
+});
