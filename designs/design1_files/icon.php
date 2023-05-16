c176ae54cff70e59bf0d4972c0b0045862278093
Agoston Reischl <ragoston001@gmail.com>
2023-05-16 22:19:17 +0200

    Project restart

---
+++ designs/design1_files/icon.php
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   id="svg130428"
+   viewBox="0 0 200 200"
+   height="200px"
+   width="200px"
+   version="1.1">
+  <metadata
+     id="metadata130434">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <defs
+     id="defs130432" />
+  <g
+     id="monogram"
+     style="font-style:normal;font-weight:normal;font-size:120px;line-height:1;font-family:'Brandmark Sans 14 Color';font-variant-ligatures:none;text-align:center;dominant-baseline:middle;text-anchor:middle;fill:#fcfcfc"
+     aria-label="A">
+    <path
+       id="path130436"
+       style="font-style:normal;font-weight:normal;font-size:120px;line-height:1;font-family:'Brandmark Sans 14 Color';font-variant-ligatures:none;text-align:center;dominant-baseline:middle;text-anchor:middle;fill:#fcfcfc"
+       d="m 142.29812,128.14 -30.12,-83.16 H 87.698125 l -30,83.16 h 23.16 l 19.08,-57.48 19.200005,57.48 z m -42.359995,-3.36 c 6.000005,0 10.799995,-5.04 10.799995,-11.16 0,-6.24 -4.79999,-11.28 -10.799995,-11.28 -6,0 -10.92,5.04 -10.92,11.28 0,6.12 4.92,11.16 10.92,11.16 z" />
+  </g>
+</svg>
